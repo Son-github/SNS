8c59bd25d9343946f6b598540275dbca20d811dc
Son <jongin718@gmail.com>
2024-02-24 18:50:05 +0900

    # Entity 파일 업로드  - UserEntity, PostEntity 파일 수정

---
+++ src/main/java/com/sonny/sns/model/Entity/PostEntity.java
@@ -0,0 +1,57 @@
+package com.sonny.sns.model.Entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+import java.sql.Timestamp;
+import java.time.Instant;
+
+@Entity
+@Table(name = "\"post\"")
+@Data
+@SQLDelete(sql = "UPDATED \"post\" SET deleted_at = NOW() where id=?")
+@Where(clause = "deleted_at is NULL")
+public class PostEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "body", columnDefinition = "TEXT")
+    private String body;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private UserEntity user;
+
+    @Column(name = "registered_at")
+    private Timestamp registerAt;
+
+    @Column(name = "updated_at")
+    private Timestamp updatedAt;
+
+    @Column(name = "deleted_at")
+    private Timestamp deletedAt;
+
+    @PrePersist // 자동으로 저장되도록
+    void registeredAt() {
+        this.registerAt = Timestamp.from(Instant.now());
+    }
+
+    @PreUpdate
+    void updatedAt() {
+        this.updatedAt = Timestamp.from(Instant.now());
+    }
+
+    public static PostEntity of(String title, String body, UserEntity userEntity){
+        PostEntity entity = new PostEntity();
+        entity.setTitle(title);
+        entity.setBody(body);
+        entity.setUser(userEntity);
+        return entity;
+    }
+}
---
+++ src/main/java/com/sonny/sns/model/Entity/UserEntity.java
@@ -0,0 +1,57 @@
+package com.sonny.sns.model.Entity;
+
+import com.sonny.sns.model.UserRole;
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+import java.sql.Timestamp;
+import java.time.Instant;
+
+@Entity
+@Data
+@Table(name = "\"user\"") // postgresql에는 이미 user라는 테이블이 있기 때문에 따움표를 쓴다.
+@SQLDelete(sql = "UPDATED \"user\" SET deleted_at = now() where id=?")
+@Where(clause = "deleted_at is NULL")
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "user_name")
+    private String userName;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "role") // 관리자인지 그냥 사용자인지 알 수 있도록 하는 것
+    @Enumerated(EnumType.STRING)
+    private UserRole role = UserRole.USER;
+
+    @Column(name = "registered_at")
+    private Timestamp registerAt;
+
+    @Column(name = "updated_at")
+    private Timestamp updatedAt;
+
+    @Column(name = "deleted_at")
+    private Timestamp deletedAt;
+
+    @PrePersist // 자동으로 저장되도록
+    void registeredAt() {
+        this.registerAt = Timestamp.from(Instant.now());
+    }
+
+    @PreUpdate
+    void updatedAt() {
+        this.updatedAt = Timestamp.from(Instant.now());
+    }
+
+    public static UserEntity of(String userName, String password) {
+        UserEntity userEntity = new UserEntity();
+        userEntity.setUserName(userName);
+        userEntity.setPassword(password);
+        return userEntity;
+    }
+}
