fb9a1b713355132b019f4f43c29c459734c99434
Son <jongin718@gmail.com>
2024-02-24 18:56:35 +0900

    # 기타 파일들 업데이트  - 설정 및 UserEntityFixture 파일 수정

---
+++ src/test/java/com/sonny/sns/fixture/UserEntityFixture.java
@@ -0,0 +1,15 @@
+package com.sonny.sns.fixture;
+
+import com.sonny.sns.model.Entity.UserEntity;
+
+public class UserEntityFixture {
+
+    public static UserEntity get(String userName, String password) {
+        UserEntity result = new UserEntity();
+        result.setId(1);
+        result.setUserName(userName);
+        result.setPassword(password);
+
+        return result;
+    }
+}
