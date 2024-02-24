87325c7134990c8a63b2407739e02642e78cd726
Son <jongin718@gmail.com>
2024-02-24 18:48:41 +0900

    # Repository 파일 수정  - UserEntity, PostEntity에 관련된 Repository 파일 수정

---
+++ src/main/java/com/sonny/sns/repository/PostEntityRepository.java
@@ -0,0 +1,9 @@
+package com.sonny.sns.repository;
+
+import com.sonny.sns.model.Entity.PostEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostEntityRepository extends JpaRepository<PostEntity, Integer> {
+}
