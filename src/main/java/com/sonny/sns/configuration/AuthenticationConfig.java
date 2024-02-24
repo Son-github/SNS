13a0ba99d939bce184548f5a48e64952392233b5
Son <jongin718@gmail.com>
2024-02-24 18:51:49 +0900

    # Config 및 Filter 파일 수정  - Config 및 Filter 파일을 수정

---
+++ src/main/java/com/sonny/sns/configuration/AuthenticationConfig.java
@@ -0,0 +1,38 @@
+package com.sonny.sns.configuration;
+
+import com.sonny.sns.configuration.Filter.JwtTokenFilter;
+import com.sonny.sns.exception.CustomAuthenticationEntryPoint;
+import com.sonny.sns.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class AuthenticationConfig {
+
+    private final UserService userService;
+    @Value("${jwt.secret-key}")
+    private String key;
+
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception { // springboot 3.xx 버전에서부터는 이렇게 변경
+        http.csrf(CsrfConfigurer::disable)
+                .authorizeHttpRequests(HttpRequest -> HttpRequest
+                        .requestMatchers("/api/*/users/join", "/api/*/users/login").permitAll()
+                        .requestMatchers("/api/**").authenticated())
+                .sessionManagement(SessionManagement -> SessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(new JwtTokenFilter(key, userService), UsernamePasswordAuthenticationFilter.class)
+                .exceptionHandling(exceptionHandling -> exceptionHandling.authenticationEntryPoint(new CustomAuthenticationEntryPoint()))
+        ;
+        return http.build();
+    }
+}
