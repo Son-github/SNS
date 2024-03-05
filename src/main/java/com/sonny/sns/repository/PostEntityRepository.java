e28a685a65181b6e1a6ec93257c88f9dd0a6138b
Son <jongin718@gmail.com>
2024-03-05 23:03:22 +0900

    # Repository 파일 수정

--- src/main/java/com/sonny/sns/repository/PostEntityRepository.java
+++ src/main/java/com/sonny/sns/repository/PostEntityRepository.java
@@ -1,9 +1,15 @@
 package com.sonny.sns.repository;
 
 import com.sonny.sns.model.Entity.PostEntity;
+import com.sonny.sns.model.Entity.UserEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PostEntityRepository extends JpaRepository<PostEntity, Integer> {
+
+    Page<PostEntity> findAllByUser(UserEntity entity, Pageable pageable);
+
 }
