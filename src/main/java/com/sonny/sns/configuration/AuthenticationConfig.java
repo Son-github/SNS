5e908ef552869f8909c3c3f6ecf12576fc614457
Son <jongin718@gmail.com>
2024-03-07 01:39:22 +0900

    # 기타 파일 수정

--- src/main/java/com/sonny/sns/configuration/AuthenticationConfig.java
+++ src/main/java/com/sonny/sns/configuration/AuthenticationConfig.java
@@ -28,9 +28,15 @@ public class AuthenticationConfig {
     private String key;
 
     @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() throws Exception{
+        return web -> web.ignoring().requestMatchers("^(?!/api/).*");
+    }
+
+    @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception { // springboot 3.xx 버전에서부터는 이렇게 변경
         http.csrf(CsrfConfigurer::disable)
                 .authorizeHttpRequests(HttpRequest -> HttpRequest
+                        .requestMatchers("/api/*/users/join", "/api/*/users/login").permitAll()
                         .requestMatchers("/api/**").authenticated())
                 .sessionManagement(SessionManagement -> SessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .addFilterBefore(new JwtTokenFilter(key, userService), UsernamePasswordAuthenticationFilter.class)
